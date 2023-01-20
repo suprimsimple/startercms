962533668665ca96121c399811a462d862c8731c
[suprimsimple] <[suprim@simple.com.au]>
2023-01-20 11:24:21 +1030

    CMS: Init

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,33 @@
+/**
+ * Alpine JS
+ * Best to put it last so that all other potential JS is available
+ * when components start getting initialized.
+ */
+import Alpine from 'alpinejs'
+import collapse from '@alpinejs/collapse'
+import focus from '@alpinejs/focus'
+import intersect from '@alpinejs/intersect'
+import breakpoint from 'alpinejs-breakpoints'
+
+Alpine.plugin(intersect)
+Alpine.plugin(breakpoint)
+Alpine.plugin(collapse)
+Alpine.plugin(focus)
+window.Alpine = Alpine
+window.AlpineBreakpointPluginBreakpointsList = [
+  'xs',
+  'sm',
+  'md',
+  'lg',
+  'xl',
+  'xxl',
+  'xxxl',
+]
+Alpine.start()
+
 // Accept HMR as per: https://vitejs.dev/guide/api-hmr.html
 if (import.meta.hot) {
   import.meta.hot.accept(() => {
-    console.log("HMR");
-  });
+    console.log('HMR')
+  })
 }
